21f3620b9b8baa685f767310afa234e3cf1a1914
mahmoud01140 <mmarouf373@gmail.com>
2025-11-04 19:23:06 +0200

    fix register student form

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api/",
+    baseURL: "https://abu-yusuf.vercel.app/api/",
     withCredentials: true,
 })
 // http://localhost:5000/api/
